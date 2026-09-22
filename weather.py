62c9e4bcf619e60347361de4e883338fc56fb768
anafberhanu877-lang <anafberhanu877@gmail.com>
2026-09-22 16:49:26 +0300

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,7 @@
+t = float(input("what is the temprature outside in celcius"))
+if 20 <= t <= 30:
+    print("you should wear a sleeve its mild tempreature")
+elif t > 30:
+    print("its hot wear a tshirt")
+else:
+    print("its cold wear a jacket")
